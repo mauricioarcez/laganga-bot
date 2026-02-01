0ea6e05b4ae409c2648a204048cbb919d81f6842
Mauricio Arce <mauricioarcez23@gmail.com>
2026-02-01 20:27:43 -0300

    first structure

---
+++ src/laganga_bot/domain/templates.py
@@ -0,0 +1,9 @@
+TWITTER_FLASH_DEAL_TEMPLATE = """🔥 OFERTA EN {source} | {discount_percent}% OFF 🔥
+
+{name} a ${current_price}
+
+{details}
+
+https://lagangaofertas.com/flash-deals/{slug}
+{image_url}
+"""
---
+++ src/laganga_bot/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from laganga-bot!")
+
+
+if __name__ == "__main__":
+    main()
